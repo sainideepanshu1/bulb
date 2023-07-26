a4f1becbcedd16a5cfa09de2a5ed10c03b8da0da
sainideepanshu1 <114898317+sainideepanshu1@users.noreply.github.com>
2023-07-26 12:56:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("on").addEventListener('dblclick',function(){
+    document.getElementById("image").src="pic_bulbon.gif";
+})
+document.getElementById("off").addEventListener('dblclick',function(){
+    document.getElementById("image").src="pic_bulboff.gif";
+})
